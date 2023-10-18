ce5fb1c3d5c85863bbff2150646fdbd61be60f69
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2023-10-18 23:11:11 +0700

    extract data from group file

--- Functions/Extract_Data_Func.h
+++ Functions/Extract_Data_Func.h
@@ -1,11 +1,40 @@
 #ifndef EXTRACTDATAFUNC_H
 #define EXTRACTDATAFUNC_H
 
-#include "../InputFiles/GroupInfo.txt"
-#include "../InputFiles/ProjectInfo.txt"
-#include "../InputFiles/SubmissionInfo.txt"
+#include <bits/stdc++.h>
+#include "../Base/Group.h"
+#include "../Base/Date.h"
 
-//! EXTRACT DATA FROM FILES
+//! EXTRACT DATA FROM GroupInfo.txt FILES
+void extractGroupInfoFile(std::vector<Group* >* groups, std::string filename)
+{ 
+    fstream fs;
+    fs.open(filename);
+    string str;
+    getline(fs, str);
+    srand((unsigned)time(0));
+    while (getline(fs, str))
+    {
 
+        istringstream ss(str);
+      
+        int id;
+        string name;
+        ss >> id;
+        if (id > groups->size())
+        {
+            groups->resize(id);
+        }
+        ss >> name;
+        (*groups)[id - 1] = new Group(name);
+        string student_name;
+        int x = 0;
 
+        while (ss >> student_name)
+        {
+            unsigned short random = rand();
+            (*groups)[id - 1]->addStudent(newStudent(student_name, random));
+        }
+    }
+}
 #endif
