20a4ef37255c75c4c2ac2b8a3897b2444937bbb1
huytoan210 <phungnguyenhuytoan@gmail.com>
2023-10-22 00:09:45 +0700

    Co-authored-by: Sad_Nguyen_Coder <SadNguyen2107@users.noreply.github.com> Co-authored-by: Tran Khanh Duy 20218123 <Duy.TK218123@sis.hust.edu.vn>

--- Functions/Project_Func.h
+++ Functions/Project_Func.h
@@ -1,9 +1,48 @@
 #ifndef PROJECTFUNC_H
 #define PROJECTFUNC_H
-
+#include <iostream>
+#include <string>
+#include <vector>
 #include "../Base/Project.h"
+#include "../Base/Date.h"
+#include "../Validate/ValidateFunc.h"
+#include "../Validate/ValidateRegex.h"
+using namespace std;
 
+// PROTOTYPE
 void inputProjectsInfo(std::vector<Project *>* projects);
 void submitProject(std::vector<Project*>* projects);
+
+void inputProjectsInfo(vector<Project *>* projects){
+    int numProjects;
+    cout << "Enter the number of projects: ";
+    cin >> numProjects;
+
+    for (int i = 1; i <= numProjects; ++i)
+    {
+        cout << "Project " << i << " Information:" << endl;
+
+        string description;
+        cout << "Description: ";
+        cin.ignore();
+        getline(cin, description);
+
+        string dueDateStr;
+        cout << "Submission Deadline (DD/MM/YYYY): ";
+        cin >> dueDateStr;
+
+        //! VALIDATE dueDate FORMAT
+        dueDateStr = getValueAfterValidate(dueDateStr, validateDate);
+
+        // CREATE Date Object
+        Date* dueDate = new Date(dueDateStr);
+
+        // Create a new Project object and add it to the vector
+        Project* project = new Project(description, dueDate);
+        projects->push_back(project);
+
+        cout << "Project " << i << " added successfully." << endl;
+    }
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "./Base/Project.h"
 #include "./Base/Group.h"
 #include "./Functions/Display_Func.h"
+#include "./Functions/Extract_Data_Func.h"
 
 int main(int argc, char const *argv[])
 {
@@ -8,16 +9,27 @@ int main(int argc, char const *argv[])
     std::vector<Group *> groups;
     std::vector<Project *> projects;
 
-    Date *date = new Date(10, 12, 2003);
-    Project *project = new Project("Hello World", date);
-    project->addSubmissionDate(4, date);
+    // Date *date = new Date(10, 12, 2003);
+    // Project *project = new Project("Hello World", date);
+    // project->addSubmissionDate(4, date);
 
-    Date *new_date = new Date(30, 12, 2004);
-    project->addSubmissionDate(10, new_date);
+    // Date *new_date = new Date(30, 12, 2004);
+    // project->addSubmissionDate(10, new_date);
 
-    projects.push_back(project);
-    // std::cout << project << std::endl;
+    // projects.push_back(project);
+    // // std::cout << project << std::endl;
 
-    displayTableTest(&groups, &projects);
+    // displayTableTest(&groups, &projects);
+    
+    extractGroupInfoFile(&groups, "InputFiles\\GroupInfo.txt");
+    for (size_t index = 0; index < groups.size(); index++)
+    {
+        if (groups[index] != nullptr)
+        {
+            std::cout << groups[index] << std::endl;
+        }
+        
+    }
+    
     return 0;
 }
