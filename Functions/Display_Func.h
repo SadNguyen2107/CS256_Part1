36a22f73211994fdc3d00fbb1e30558492777c87
SimonTranDangCode <Duycucai2014@gmail.com>
2023-10-23 20:51:21 +0700

    SAP XONG VIEC ROI

--- Functions/Display_Func.h
+++ Functions/Display_Func.h
@@ -7,7 +7,6 @@
 
 void displayByProject(std::vector<Group *> *groups, std::vector<Project *> *projects);
 void displayByGroup(std::vector<Group *> *groups, std::vector<Project *> *projects);
-void showSubmissionToASpecificDate(std::vector<Group *> *groups, std::vector<Project *> *projects);
 void displayAllTable(std::vector<Group *> *groups, std::vector<Project *> *projects);
 std::queue<Group *>* findGroupsNotCompleteOnTime(std::vector<Group *> *groups, std::vector<Project *> *projects);
 std::queue<Group *>* findGroupsCompleteOnTime(std::vector<Group *> *groups, std::vector<Project *> *projects);
