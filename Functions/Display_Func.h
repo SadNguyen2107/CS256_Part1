d31ae3a934907b4eb161a0bd7e10d73d5bd88a7e
SimonTranDangCode <Duycucai2014@gmail.com>
2023-10-25 12:05:48 +0700

    ko ok

--- Functions/Display_Func.h
+++ Functions/Display_Func.h
@@ -14,24 +14,6 @@ void displayAllTable(std::vector<Group *> *groups, std::vector<Project *> *proje
 std::queue<Group *> *findGroupsNotCompleteOnTime(std::vector<Group *> *groups, std::vector<Project *> *projects);
 std::queue<Group *> *findGroupsCompleteOnTime(std::vector<Group *> *groups, std::vector<Project *> *projects);
 
-void displayByGroup(std::vector<Group *> *groups, std::vector<Project *> *projects)
-{
-  std::cout << "Enter the group index: ";
-  int group_index;
-  std::cin >> group_index;
-
-  for (Project *project : *projects)
-  {
-    std::vector<Date *> submission_dates = project->getSubmissionDateCopy();
-    Date *submission_date_group = submission_dates[group_index - 1];
-
-    std::cout << "Project name: " << project->getDescription() << std::endl;
-    std::cout << "Submission date: " << submission_date_group << std::endl;
-    std::cout << "State: " << checkState(project->getDueDate(), submission_date_group) << std::endl;
-    std::cout << std::endl;
-  }
-}
-
 void displayAllTable(std::vector<Group *> *groups, std::vector<Project *> *projects)
 {
   // string specificDate_string = "";
@@ -86,7 +68,7 @@ void displayAllTable(std::vector<Group *> *groups, std::vector<Project *> *proje
       std::vector<Date *> submission_dates = project->getSubmissionDateCopy();
       for (std::vector<Project *>::size_type project_index = 0; project_index < projects->size(); project_index++)
       {
-        std::cout << "|" << std::left << std::setw(8) << checkState(project->getDueDate(), submission_dates[project_index]);
+        std::cout << "|" << std::left << std::setw(9) << checkState(project->getDueDate(), submission_dates[project_index]);
       }
       std::cout << std::endl;
       std::cout << "+======================================================================================+\n";
