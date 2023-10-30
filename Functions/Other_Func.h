bb0592cb2b2d064767427322f3a36441e1f6c5ce
SimonTranDuy <duycucai2014@gmail.com>
2023-10-30 22:00:09 +0700

    ok chua SON

--- Functions/Other_Func.h
+++ Functions/Other_Func.h
@@ -8,12 +8,14 @@
 #include "../Validate/ValidateRegex.h"
 #include "../Validate/ValidateFile.h"
 #include "../Base/Project.h"
+#include "../Base/Group.h"
 
 int askUserNumberOfGroups();
 int askUserNumberOfProjects();
 int askGroupIDToSubmit();
 int askProjectIDToSubmit();
-std::string askUserFileDirectory();
+std::string askUserFileGroupsDirectory();
+std::string askUserFileProjectsDirectory();
 void quitProgram();
 
 int askUserNumberOfGroups()
@@ -80,72 +82,49 @@ int askProjectIDToSubmit()
 
     return projectID;
 }
-std::string askUserFileDirectory()
+std::string askUserFileGroupsDirectory()
 {
     string fileGroups = "";
-    string fileProjects = "";
-    int option = 0;
-    int groupsInfo = 0;
 
     std::vector<Group *> groups;
+
+    // ASK PEOPLE FILE PATH GROUPS
+    cout << "Enter groups file Path: ";
+    std::cin >> fileGroups;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    while (!isRightFile(fileGroups, GROUP_INFO_FILE))
+    {
+        std::cout << "Wrong file! Enter groups file Path Again: ";
+        std::getline(std::cin, fileGroups);
+    }
+    //! VAILIDATE FILE PATH GROUPS
+    fileGroups = getValueAfterValidate(fileGroups, validateFileTxt);
+    std::cout << fileGroups << " has been loaded successfully!\n";
+
+    return fileGroups;
+}
+std::string askUserFileProjectsDirectory()
+{
+    string fileProjects = "";
+
     std::vector<Project *> projects;
 
-    // ASK INPUT
-    cout << "Load corporation from file(1) or input from terminal(2)? ";
-    std::cin >> option;
-    while (!(option == 1 || option == 2))
+    // ASK PEOPLE FILE PATH PROJECTS
+    cout << "Enter projects file Path: ";
+    std::cin >> fileProjects;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    while (!isRightFile(fileProjects, GROUP_INFO_FILE))
     {
-        std::cout << "Enter the option again!!!!!!" << endl;
-        std::cin >> option;
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Wrong file! Enter projects file Path Again: ";
+        std::getline(std::cin, fileProjects);
     }
-    // Do File Path
-    if (option == 1)
-    {
-        // ASK PEOPLE FILE PATH GROUPS
-        cout << "Enter groups file Path: ";
-        std::cin >> fileGroups;
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    //! VAILIDATE FILE PATH PROJECTS
+    fileProjects = getValueAfterValidate(fileProjects, validateFileTxt);
+    std::cout << fileProjects << " has been loaded successfully!\n";
 
-        while (!isRightFile(fileGroups, GROUP_INFO_FILE))
-        {
-            cout << "Wrong file! Enter groups file Path Again: ";
-            std::getline(std::cin, fileGroups);
-        }
-        //! VAILIDATE FILE PATH GROUPS
-        fileGroups = getValueAfterValidate(fileGroups, validateFileTxt);
-        std::cout << fileGroups << " has been loaded successfully!\n";
-
-        // ASK PEOPLE FILE PATH PROJECTS
-        std::cout << "Enter projects file Path: ";
-        std::cin >> fileProjects;
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-        while (!isRightFile(fileProjects, PROJECT_INFO_FILE))
-        {
-            cout << "Wrong file! Enter projects file Path Again: ";
-            std::getline(std::cin, fileProjects);
-        }
-        //! VAILIDATE FILE PATH PROJECTS
-        fileProjects = getValueAfterValidate(fileProjects, validateFileTxt);
-        std::cout << fileProjects << " has been loaded successfully!\n";
-
-        //! PREVENT MEMORY LEAK
-        delete &groups;
-        delete &projects; 
-        // Load data from file
-        groupsInfo = extractGroupInfoFile(&groups, fileGroups);
-        extractProjectInfoFile(&projects, groupsInfo,fileProjects);
-
-    }
-    // Do Terminal
-    else if (option == 2)
-    {
-        delete &groups;
-        delete &projects;  //! PREVENT MEMORY LEAK
-        groupsInfo = extractGroupInfoFile(&groups, fileGroups);
-        extractProjectInfoFile(&projects, groupsInfo,fileProjects);
-    }
+    return fileProjects;
 }
 void quitProgram()
 {
